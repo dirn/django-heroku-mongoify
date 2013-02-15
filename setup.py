56be0e9d48d0620199ab6f8d97ba4575644f0dc1
Andy Dirnberger <dirn@dirnonline.com>
2013-02-15 18:07:47 -0500

    Make classifiers a list
    PyPi doesn't seem to like when you use tuples

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ settings.update(
     include_package_data=True,
     tests_require=['coverage', 'nose'],
     license=open('LICENSE').read(),
-    classifiers=(
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
@@ -40,7 +40,7 @@ settings.update(
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules',
-    ),
+    ],
 )
 
 setup(**settings)
